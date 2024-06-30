76db3ad55c0ed3ac6df933fb0d72d120f67ed8ad
Hector Vazquez <hector.vazquez@jaesis.com>
2024-06-29 23:10:32 -0400

    Fixed format

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Typography, Button } from '@mui/material';
 import './LandingPage.css'; // Import the CSS file for styling
 
 // Import your logos
@@ -12,11 +13,23 @@ import dbLogo4 from '../public/Mysql_Logo.svg';
 
 const LandingPage = () => {
   return (
+    <>
+    <CssBaseline />
     <div className="landing-page">
       <div className="main-content">
         <img src={appLogo} alt="App Logo" className="app-logo" />
         <h1 className="headline"><span class="main-color">AI-Accelerated</span><br></br>Database Modeling</h1>
+        <Button variant="outlined">Get Started</Button>
       </div>
+      <Typography
+        component="p"
+        align="center"
+        paddingBottom={1}
+        variant="overline"
+        color="lightgray"
+      >
+        Supported Data Platforms
+      </Typography>
       <div className="footer-logos">
         <img src={dbLogo1} alt="Database Logo 1" className="db-logo" />
         <img src={dbLogo2} alt="Database Logo 2" className="db-logo" />
@@ -24,6 +37,7 @@ const LandingPage = () => {
         <img src={dbLogo4} alt="Database Logo 4" className="db-logo" />
       </div>
     </div>
+    </>
   );
 };
 
