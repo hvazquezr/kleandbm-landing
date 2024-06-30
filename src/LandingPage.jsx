951360bb1bd56c519f1fd07a0326c42188fe6dae
Hector Vazquez <hector.vazquez@jaesis.com>
2024-06-30 00:04:26 -0400

    Fine tuned look and feel

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Typography, Button } from '@mui/material';
+import { styled } from '@mui/system';
 import './LandingPage.css'; // Import the CSS file for styling
 
 // Import your logos
@@ -9,6 +10,21 @@ import dbLogo1 from '/Snowflake_Logo.svg';
 import dbLogo2 from '/Databricks_Logo.svg';
 import dbLogo3 from '/SQLServer_Logo.png';
 import dbLogo4 from '/Mysql_Logo.svg';
+
+const CustomButton = styled(Button)(({ theme }) => ({
+  borderRadius: '0px', // Squared corners
+  borderWidth: '2px', // Thicker outline
+  borderStyle: 'solid', // Ensure the border is solid
+  fontWeight: 'bold', // Bold inner text
+  fontFamily: 'Roboto, sans-serif', // Custom font family
+  color: '#1cb8f5', // Custom text color
+  borderColor: '#1cb8f5', // Custom border color
+  '&:hover': {
+    borderWidth: '2px', // Maintain thicker outline on hover
+    backgroundColor: 'rgba(28, 184, 245, 0.1)', // Light background on hover
+    borderColor: '#1cb8f5', // Custom border color
+  },
+}));
 
 
 const LandingPage = () => {
@@ -19,12 +35,12 @@ const LandingPage = () => {
       <div className="main-content">
         <img src={appLogo} alt="App Logo" className="app-logo" />
         <h1 className="headline"><span class="main-color">AI-Accelerated</span><br></br>Database Modeling</h1>
-        <Button
+        <CustomButton
           variant="outlined"
           component="a"
           href="https://app.kleandbm.ai/dashboard"
           size="large"
-        >Get Started</Button>
+        >Get Started</CustomButton>
       </div>
       <Typography
         component="p"
